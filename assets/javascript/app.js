43c3644be9b29ed498e2f783d241dbb48ed2e8e7
edgarftp <edgarftp@gmail.com>
2018-05-26 12:04:30 -0500

    display works, needs CSS

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -26,5 +26,36 @@ var config = {
         dateAdded: firebase.database.ServerValue.TIMESTAMP
       });
 
-    
   })
+
+  database.ref("/user").on("child_added", function(snapshot) {
+    var sv = snapshot.val();
+    console.log(sv);
+
+        var newDiv = $("<div>");
+        newDiv.addClass("row col-12 col-lg-12");
+        var namedb = $("<p>").text(sv.name);
+        namedb.attr("class", "col-4 col-lg-4 d-inline");
+        var roledb = $("<p>").text(sv.role);
+        roledb.attr("class", "col-1 col-lg-1 d-inline");
+        var datedb = $("<p>").text(sv.date);
+        datedb.attr("class", "col-2 col-lg-2 d-inline");
+        var months = $("<p>").text(6);
+        months.attr("class", "col-1 col-lg-1 d-inline");
+        var ratedb = $("<p>").text(sv.rate);
+        ratedb.attr("class", "col-2 col-lg-2 d-inline");
+        var billed = $("<p>").text(10000);
+        billed.attr("class", "col-2 col-lg-2 d-inline");
+        newDiv.append(namedb);
+        newDiv.append(roledb);
+        newDiv.append(datedb);
+        newDiv.append(months);
+        newDiv.append(ratedb);
+        newDiv.append(billed);
+        console.log(newDiv);
+        $("#employee-holder").append(newDiv);
+        
+
+        
+})
+ 
